1f9a13caad54aeeba33f07245fce8d2931128899
Joey J <joey@wyxos.com>
2025-03-24 03:11:38 +0400

    feat: update CNAME file to point to vue-infinite-masonry.wyxos.com

--- write-cname.js
+++ write-cname.js
@@ -1,5 +1,5 @@
 // scripts/write-cname.js
 import { writeFile } from 'fs/promises';
 
-await writeFile('dist/CNAME', 'masonry.wyxos.com');
+await writeFile('dist/CNAME', 'vue-infinite-masonry.wyxos.com');
 console.log('✅ CNAME file written to dist/');
