448cd7a137064ebaf3b3f92305b30f2faa5a0873
Joey J <joey@wyxos.com>
2025-03-24 02:15:49 +0400

    feat: set base path for deployment and reorder plugins in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/vue-infinite-masonry/',
     plugins: [
-        tailwindcss(),
         vue(),
+        tailwindcss(),
     ],
 })
