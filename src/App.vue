0c8652f425b4ad3627f165a7121302fa1ad63b68
Joey J <joey@wyxos.com>
2025-03-24 01:30:06 +0400

    add InfiniteMasonry component and implement infinite loading functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,58 @@
 <script setup>
+import InfiniteMasonry from "./InfiniteMasonry.vue";
+import {onMounted, ref} from "vue";
 
+const files = ref([]);
+
+const load = async () => {
+  // create an array of object with uuid and an image with random dimensions
+  const data = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Array.from({length: 60}, (_, index) => ({
+        id: index,
+        index: index,
+        src: `https://picsum.photos/${Math.floor(Math.random() * 200) + 200}/${Math.floor(Math.random() * 200) + 200}?random=${index}`
+      })))
+    }, 1500)
+  })
+
+  files.value = data;
+}
+
+const loadNext = async () => {
+  const currentLength = files.value.length;
+
+
+  const data = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Array.from({length: 60}, (_, index) => ({
+        id: index + currentLength,
+        index: index + currentLength,
+        src: `https://picsum.photos/${Math.floor(Math.random() * 200) + 200}/${Math.floor(Math.random() * 200) + 200}?random=${index + currentLength}`
+      }))
+      )
+    }, 1500)
+  })
+
+  files.value = [...files.value, ...data];
+}
+
+onMounted(async () => {
+});
 </script>
 
 <template>
   <div class="flex flex-col items-center p-4 bg-slate-100 min-h-screen">
-    <h1 class="text-2xl font-semibold">Vue infinite masonry</h1>
+    <h1 class="text-2xl font-semibold mb-6">Vue infinite masonry</h1>
+
+    <infinite-masonry v-model="files" :callbacks="{ load, loadNext}">
+      <template #item="{ item, items, column }">
+        <div class="bg-slate-500 min-h-30">
+          <img :src="item.src" class="w-full h-auto" />
+          {{ item.index}}
+        </div>
+        {{ items[column - 1]?.length }}
+      </template>
+    </infinite-masonry>
   </div>
 </template>
---
+++ src/InfiniteMasonry.vue
@@ -0,0 +1,143 @@
+<script setup>
+
+import {computed, nextTick, onMounted, onUnmounted, ref} from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    default(){
+      return [];
+    }
+  },
+  callbacks: {
+    load: {
+      type: Function,
+      required: true
+    },
+    loadNext: {
+      type: Function,
+      required: true
+    },
+  },
+  sizes: {
+    type: Object,
+    default(){
+      return {
+        0: 1,
+        640: 2,
+        768: 3,
+        1024: 4,
+        1280: 5,
+        1536: 6,
+        1792: 7,
+        2048: 8,
+      }
+    }
+  }
+})
+
+const columns = ref(6);
+const isLoading = ref(false);
+const isLoadingNext = ref(false);
+
+const containerSize = ref(0);
+
+const getContainerClasses = computed(() => {
+  return `grid-cols-${columns.value}`;
+});
+
+const groupedItems = computed(() => {
+  return props.modelValue.reduce((acc, item, index) => {
+    const n = index % columns.value;
+    if (!acc[n]) {
+      acc[n] = [];
+    }
+    acc[n].push(item);
+    return acc;
+  }, [])
+})
+
+const visibleColumnEnds = ref(new Set());
+
+onMounted(async () => {
+  updateContainerSize();
+  window.addEventListener('resize', updateContainerSize);
+
+  isLoading.value = true;
+  await props.callbacks.load();
+  isLoading.value = false;
+  await nextTick();
+  observeColumnEnds();
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateContainerSize);
+  if (observer) observer.disconnect();
+});
+
+const updateContainerSize = () => {
+  const width = window.innerWidth;
+  const newSize = Object.keys(props.sizes).reduce((acc, size) => {
+    return width >= Number(size) ? size : acc;
+  }, 0);
+
+  if (newSize !== containerSize.value) {
+    containerSize.value = newSize;
+    columns.value = props.sizes[newSize];
+  }
+};
+
+let observer = null;
+
+const observeColumnEnds = () => {
+  if (observer) observer.disconnect(); // clear previous one
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const column = Number(entry.target.getAttribute('data-column'));
+      if (entry.isIntersecting) {
+        visibleColumnEnds.value.add(column);
+      } else {
+        visibleColumnEnds.value.delete(column);
+      }
+    });
+
+    if (visibleColumnEnds.value.size > 0 && !isLoadingNext.value) {
+      isLoadingNext.value = true;
+
+      props.callbacks.loadNext().then(() => {
+        isLoadingNext.value = false;
+      });
+    }
+  });
+
+  groupedItems.value.forEach((group, index) => {
+    const lastItem = document.querySelector(`[data-column="${index}"][data-last="true"]`);
+    if (lastItem) {
+      observer.observe(lastItem);
+    }
+  });
+};
+</script>
+
+<template>
+  <div class="grid w-full gap-4" :class="getContainerClasses">
+    <div v-for="column in columns" :key="column" class="flex flex-col gap-4">
+      <div v-for="(item, index) in groupedItems[column - 1]" :key="item.id"
+                :data-column="column - 1"
+                :data-last="index === groupedItems[column - 1].length - 1">
+        <slot name="item"
+              :item="item"
+              :index="index"
+              :column="column"
+              :items="groupedItems[column - 1]">
+          <!-- Default / fallback content -->
+          <div class="bg-slate-300 w-full rounded-lg shadow-lg p-4"
+               :style="{ height: `${Math.floor(Math.random() * 200) + 200}px` }">
+            {{ `item-${item.index}` }}
+          </div>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
