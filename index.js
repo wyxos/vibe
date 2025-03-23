f3290b66f54e201c40015e30565f40c97cccf472
Joey J <joey@wyxos.com>
2025-03-24 01:33:33 +0400

    add index.js for InfiniteMasonry component and update package.json for naming and dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+import InfiniteMasonry from './src/InfiniteMasonry.vue';
+
+export default {
+    install(app) {
+        app.component('InfiniteMasonry', InfiniteMasonry);
+    }
+};
+
+export { InfiniteMasonry };
