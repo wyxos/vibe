68ab7c445a969f332e767ddf64decf087a5663c2
Joey J <joey@wyxos.com>
2025-03-23 19:03:36 +0400

    initialize Vue 3 + Vite project with Tailwind CSS and essential files

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center p-4 bg-slate-100 min-h-screen">
+    <h1 class="text-2xl font-semibold">Vue infinite masonry</h1>
+  </div>
+</template>
