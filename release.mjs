5ba442c9da7fd3c9de980911d624223a4484cc77
Joey J <joey@wyxos.com>
2025-03-24 01:52:27 +0400

    update package.json and package-lock.json to downgrade Vue version and add simple-git as a dev dependency

--- release.mjs
+++ release.mjs
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
-import { execSync } from "child_process";
+import {execSync} from "child_process";
 import simpleGit from "simple-git";
 import fs from "fs";
 
@@ -14,33 +14,50 @@ let defaultVersion = currentVersion.split(".");
 defaultVersion[defaultVersion.length - 1] = Number(defaultVersion[defaultVersion.length - 1]) + 1;
 defaultVersion = defaultVersion.join(".");
 
-const { version } = await inquirer.prompt([
-  {
-    name: "version",
-    message: `Enter the version to publish (current ${currentVersion})`,
-    default: defaultVersion
-  }
+const {version} = await inquirer.prompt([
+    {
+        name: "version",
+        message: `Enter the version to publish (current ${currentVersion})`,
+        default: defaultVersion
+    }
 ]);
 
 const tagVersion = `v${version}`;
 const commitMessage = `feat: release ${tagVersion}`;
 
 const execSyncOut = (command) => {
-  execSync(command, { stdio: "inherit" });
+    execSync(command, {stdio: "inherit"});
 };
 
-// Run linting
-execSyncOut("npm run lint");
+// check if directory clean
+const status = await git.status();
+if (status.files.length > 0) {
+    // list files and ask for commit message
+    console.log(chalk.red("You have uncommitted changes"));
+    console.log(status.files);
+    const {commitMessage} = await inquirer.prompt([
+        {
+            name: "commitMessage",
+            message: "Enter the commit message"
+        }
+    ]);
+    await git.add(".");
+    await git.commit(commitMessage);
 
-// Build the project
-execSyncOut("npm run build");
+}
+
+// if script has lint
+if (packageJson.scripts.lint) {
+// Run linting
+    execSyncOut("npm run lint");
 
 // Check for changes
-const status = await git.status();
-if (status.modified.length > 0) {
-  await git.add(".");
-  // Commit the changes
-  await git.commit("chore: linted");
+    const status = await git.status();
+    if (status.modified.length > 0) {
+        await git.add(".");
+        // Commit the changes
+        await git.commit("chore: lint fixes");
+    }
 }
 
 // Build the project
@@ -50,26 +67,26 @@ execSyncOut("npm run build");
 execSyncOut(`npm version ${version} -m "${commitMessage}"`);
 
 const commitFiles = async () => {
-  await git.add(".");
-  await git.commit(commitMessage);
+    await git.add(".");
+    await git.commit(commitMessage);
 };
 
 const pushChanges = async () => {
-  await git.push("origin", "main");
-  await git.pushTags("origin");
+    await git.push("origin", "main");
+    await git.pushTags("origin");
 };
 
 const release = async () => {
-  try {
-    await commitFiles();
-    await pushChanges();
-    console.log(chalk.green(`Successfully released version ${version}`));
-    console.log(chalk.green("Publishing to npm..."));
-    // execSyncOut("npm login");
-    execSyncOut("npm publish --access public --verbose");
-  } catch (error) {
-    console.error(chalk.red("Release process failed. Error:", error));
-  }
+    try {
+        await commitFiles();
+        await pushChanges();
+        console.log(chalk.green(`Successfully released version ${version}`));
+        console.log(chalk.green("Publishing to npm..."));
+        // execSyncOut("npm login");
+        execSyncOut("npm publish --access public --verbose");
+    } catch (error) {
+        console.error(chalk.red("Release process failed. Error:", error));
+    }
 };
 
 release();
